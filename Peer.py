4e43254c92549f979fe2cf62f6bc0ac3695cf9a1
Victor B <victor_luizbernardes5@outlook.com>
2022-07-28 01:09:48 -0300

    minors

--- Peer.py
+++ Peer.py
@@ -21,7 +21,7 @@ class Peer:
 
     @staticmethod
     def getInternalIp():
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s = socket(AF_INET, SOCK_DGRAM)
         s.connect(('8.8.8.8', 80))
         ip = s.getsockname()[0]
         s.close()
